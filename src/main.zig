e6545e8b760d3658014940f880c3450fa2c4849c
Vitaly _Vi Shukela <vi0oss@gmail.com>
2019-10-06 12:36:29 +0300

    Refactor CLI

--- src/main.zig
+++ src/main.zig
@@ -1,68 +1,27 @@
 const std = @import("std");
 const warn = std.debug.warn;
 
-const mkv = @import("mkv.zig");
-
-fn handler(indent : *usize, ev: mkv.L2Parser.Event) anyerror!void {
-    if (ev == .unknown_or_void_chunk) {
-        return;
-    }
-    var ugly_counter = indent.*; // Zig should have better syntax for repeating
-    while(ugly_counter>0):(ugly_counter-=1) {
-        warn("  ");
-    }
-    switch(ev) {
-        .element_begins => |x| {
-            warn("open 0x{x} ({}) type={} size={}\n", x.id.id, x.id.get_name(), x.typ, x.size);
-            indent.* += 1;
-        },
-        .number => |x|{
-            warn("number {}\n", x);
-        },
-        .signed_number => |x|{
-            warn("signed_number {}\n", x);
-        },
-        .binary_chunk => |x|{
-            warn("binary chunk len={}\n", x.len);
-        },
-        .unknown_or_void_chunk => unreachable,
-        .string_chunk => |x|{
-            warn("string {}\n", x);
-        },
-        .utf8_chunk => |x|{
-            warn("utf8 {}\n", x);
-        },
-        .float => |x| {
-            warn("float {}\n", x);
-        },
-        .element_ends => |x| {
-            warn("close 0x{x} ({}) type={}\n", x.id.id, x.id.get_name(), x.typ);
-            indent.* -= 1;
-        },
-    }
-}
+var pool : [4096]u8 = undefined;
 
 pub fn main() anyerror!void {
-    var si = try std.io.getStdIn();
-
-    var buf : [256]u8 = undefined;
-
-    var m = mkv.L2Parser.new();
-
-    var indent : usize = 0;
-
-    while(true) {
-        var ret = si.read(buf[0..]) catch |e| {
-            if (e == error.WouldBlock) {
-                std.time.sleep(50*1000*1000);
-                continue;
-            }
-            return e;
-        };
-        if (ret == 0) break;
-        const b = buf[0..ret];
-        try m.push_bytes(b, &indent, handler);
+    var alloc = std.heap.FixedBufferAllocator.init(&pool);
+    const argv = try std.process.argsAlloc(&alloc.allocator);
+    defer std.process.argsFree(&alloc.allocator, argv);
+    
+    if (argv.len<2) {
+        warn("Usage: zigmkv {{help|<subtool_name>}} ...\n");
+        std.process.exit(1);
     }
-    warn("OK\n");
 
+    if (std.mem.eql(u8, argv[1], "help")) {
+        warn("Tools:\n");
+        warn("  zigmkv l2dump\n");
+        warn("Use --help option after tool name for further help.\n");
+    }
+    else if (std.mem.eql(u8, argv[1], "l2dump")) {
+        try @import("tool_l2dump.zig").tool(argv[2..]);
+    } else {
+        warn("Unknown tool name `{}`\n", argv[1]);
+        std.process.exit(1);
+    }
 }
---
+++ src/tool_l2dump.zig
@@ -0,0 +1,75 @@
+const std = @import("std");
+const warn = std.debug.warn;
+
+const mkv = @import("mkv.zig");
+
+fn handler(indent : *usize, ev: mkv.L2Parser.Event) anyerror!void {
+    if (ev == .unknown_or_void_chunk) {
+        return;
+    }
+    var ugly_counter = indent.*; // Zig should have better syntax for repeating
+    while(ugly_counter>0):(ugly_counter-=1) {
+        warn("  ");
+    }
+    switch(ev) {
+        .element_begins => |x| {
+            warn("open 0x{x} ({}) type={} size={}\n", x.id.id, x.id.get_name(), x.typ, x.size);
+            indent.* += 1;
+        },
+        .number => |x|{
+            warn("number {}\n", x);
+        },
+        .signed_number => |x|{
+            warn("signed_number {}\n", x);
+        },
+        .binary_chunk => |x|{
+            warn("binary chunk len={}\n", x.len);
+        },
+        .unknown_or_void_chunk => unreachable,
+        .string_chunk => |x|{
+            warn("string {}\n", x);
+        },
+        .utf8_chunk => |x|{
+            warn("utf8 {}\n", x);
+        },
+        .float => |x| {
+            warn("float {}\n", x);
+        },
+        .element_ends => |x| {
+            warn("close 0x{x} ({}) type={}\n", x.id.id, x.id.get_name(), x.typ);
+            indent.* -= 1;
+        },
+    }
+}
+
+var pool : [4096]u8 = undefined;
+
+pub fn tool(argv:[][]const u8) anyerror!void {
+
+    if (argv.len > 0) {
+        warn("Usage: zigmkv l2dump < input_file.mkv\n");
+        std.process.exit(1);
+    }
+
+    var si = try std.io.getStdIn();
+
+    var buf : [256]u8 = undefined;
+
+    var m = mkv.L2Parser.new();
+
+    var indent : usize = 0;
+
+    while(true) {
+        var ret = si.read(buf[0..]) catch |e| {
+            if (e == error.WouldBlock) {
+                //std.time.sleep(50*1000*1000); // fails on WASI
+                continue;
+            }
+            return e;
+        };
+        if (ret == 0) break;
+        const b = buf[0..ret];
+        try m.push_bytes(b, &indent, handler);
+    }
+    warn("OK\n");
+}
